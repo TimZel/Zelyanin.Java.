cd56353cbd34114727de44ec62598cb58f894cd3
TimZel <107506944+TimZel@users.noreply.github.com>
2022-08-30 15:14:29 +0300

    Update task2.java

--- src/tasks/task2.java
+++ src/tasks/task2.java
@@ -2,19 +2,18 @@ package tasks;
 
 import java.util.Scanner;
 
-public class task2
-{
+public class task2 {
     public static void main(String[] args) {
     Scanner console = new Scanner(System.in);
     System.out.println("введите Ваше имя:");
     String name = console.nextLine();
-	    if (name.equals("Вячеслав")) {
-	    	System.out.println("Привет, " + name);
-	    } else { 
-	    	System.out.println("Нет такого имени!" + "\n" + 
-	    			           "введите Ваше имя:");
-	    	output(name);
-	    }
+	 if (name.equals("Вячеслав")) {
+	 	System.out.println("Привет, " + name);
+	 } else { 
+	 	System.out.println("Нет такого имени!" + "\n" + 
+	    	            	   "введите Ваше имя:");
+	 	output(name);
+	 }
     console.close();     
     }
     public static void output(String name) {
