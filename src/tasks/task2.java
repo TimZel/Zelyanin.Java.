61ff1c03f5b89812e14d44501feb13f2ddd8e768
TimZel <107506944+TimZel@users.noreply.github.com>
2022-08-30 15:10:41 +0300

    Update task2.java

--- src/tasks/task2.java
+++ src/tasks/task2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class task2
 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
     Scanner console = new Scanner(System.in);
     System.out.println("введите Ваше имя:");
     String name = console.nextLine();
