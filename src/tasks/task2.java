29175ad3848ed7574574d7077a25de7425d667c8
TimZel <107506944+TimZel@users.noreply.github.com>
2022-08-30 15:08:05 +0300

    Update task2.java

--- src/tasks/task2.java
+++ src/tasks/task2.java
@@ -5,15 +5,30 @@ import java.util.Scanner;
 public class task2
 {
 	public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("введите Ваше имя");
-        String name = console.next();
-        console.close();
-        if (name.equals("Вячеслав")) {
-	        System.out.println("Привет, " + name);
-        } else { 
-            System.out.println("Нет такого имени");
-        }
-        console.close();
+    Scanner console = new Scanner(System.in);
+    System.out.println("введите Ваше имя:");
+    String name = console.nextLine();
+	    if (name.equals("Вячеслав")) {
+	    	System.out.println("Привет, " + name);
+	    } else { 
+	    	System.out.println("Нет такого имени!" + "\n" + 
+	    			           "введите Ваше имя:");
+	    	output(name);
+	    }
+    console.close();     
     }
-}      
+    public static void output(String name) {
+    Scanner consoleNext = new Scanner(System.in);
+         while(consoleNext.hasNextLine()) {
+         name = consoleNext.nextLine();
+         	if (name.equals("Вячеслав")) {
+            	    System.out.println("Привет, " + name);
+            	    break;
+            	} else {
+            	    System.out.println("Такого имени нет!" + "\n" + 
+            	                       "введите Ваше имя:");
+            	}   
+         }
+    consoleNext.close();   	
+    }    
+}  
