41a17476e672534affba9070375eede31c1e9ebd
TimZel <107506944+TimZel@users.noreply.github.com>
2022-08-30 15:17:40 +0300

    Update task1.java

--- src/tasks/task1.java
+++ src/tasks/task1.java
@@ -2,30 +2,29 @@ package tasks;
 
 import java.util.Scanner;
 
-public class task1
-{
+public class task1 {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("введите целое число от -100 до 100");
-        int a = console.nextInt();
+    Scanner console = new Scanner(System.in);
+    System.out.println("введите целое число от -100 до 100");
+    int a = console.nextInt();
         if (a > 7) {
-	        System.out.println("Привет");
+	    System.out.println("Привет");
         } else { 
             System.out.println("введите другое число");
-        output(a);
+            output(a);
         }
-        console.close();
+    console.close();
     }
     public static void output(int a) {
     Scanner consoleNext = new Scanner(System.in);
     	while(consoleNext.hasNextInt()) {
     	a = consoleNext.nextInt();
-        	if (a > 7) {
-        	    System.out.println("Привет");
-        	    break;
-        	} else {
-        	    System.out.println("введите другое число");
-        	}   
+           if (a > 7) {
+              System.out.println("Привет");
+              break;
+           } else {
+              System.out.println("введите другое число");
+           }   
     	}
     consoleNext.close();	
     }
