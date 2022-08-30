0e6a37f446e9daf5b5d3a3168e3a8506d1c62289
TimZel <107506944+TimZel@users.noreply.github.com>
2022-08-30 15:12:12 +0300

    Update task3.java

--- src/tasks/task3.java
+++ src/tasks/task3.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class task3
 {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("введите длину массива: ");
-        int [] array = new int[console.nextInt()];
-        System.out.println("\n" + "введите " + array.length +  " целых числа(ел) для заполнения массива:");
+    Scanner console = new Scanner(System.in);
+    System.out.println("введите длину массива: ");
+    int [] array = new int[console.nextInt()];
+    System.out.println("\n" + "введите " + array.length +  " целых числа(ел) для заполнения массива:");
         for (int i = 0; i < array.length; i++) {
             array[i] = console.nextInt();
         }
-        System.out.print("\n" + "элементы массива, кратные 3: ");
-        console.close();
+    System.out.print("\n" + "элементы массива, кратные 3: ");
+    console.close();
         for (int i = 0; i < array.length; i++) {
             if(array[i] % 3 == 0) {
                  System.out.print(array[i] + " ");
             }
-        }
+        }   
     }
 }    
     
